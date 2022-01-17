b70f54de30a26ae050e6501015b38e938d03e81a
Loskutnikov Artem <artlosk@gmail.com>
2022-01-17 16:09:21 +0600

    add lesson11

---
+++ lesson11/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:hero_card/space.dart';
+import 'main_view.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MainView(),
+    );
+  }
+}
